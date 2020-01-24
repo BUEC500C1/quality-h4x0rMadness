a3178f3dfbc152871eb81ed0138e676861c37b4d
h4x0rMadness <robertmorrislike@gmail.com>
2020-01-24 14:18:28 -0500

    Add files via upload

---
+++ test_file1.py
@@ -0,0 +1,10 @@
+import numpy
+
+def plus_one(x):
+    return x + 1
+
+def test_method():
+    assert plus_one(3) == 3 + 2, "test failed because " + str(plus_one(3)) + " is not equal to " + str(3 + 1)
+
+
+
